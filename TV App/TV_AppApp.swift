1b439fd6c23498504523707e52cc664cc78cd8bb
MonzerMahmoud <55148623+MonzerMahmoud@users.noreply.github.com>
2022-06-17 10:19:37 +0200

    Initial Commit

---
+++ TV App/TV_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TV_AppApp.swift
+//  TV App
+//
+//  Created by Syber Expertise on 17/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct TV_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
